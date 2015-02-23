e63efe56c1f09f251952e749294ad4098399360d
Dane Larsen <dane.t.larsen@gmail.com>
2015-02-23 04:11:48 +0000

    demonstrate all microstep levels

--- Makefile
+++ Makefile
@@ -1,18 +1,19 @@
 CFLAGS = -Wall -g -std=gnu99 -pthread
+LIBS = -lm
 
 all: stepper_control 
 
 stepper_control: main.c libstepper.so
-	gcc $(CFLAGS) main.c -fPIC -o stepper_control -lrt -lstepper
+	gcc $(CFLAGS) main.c -fPIC -o stepper_control $(LIBS) -lstepper
 
 libstepper.so: gpio.o stepper.o
-	gcc -shared -fPIC gpio.o stepper.o -o libstepper.so -lrt
+	gcc -shared -fPIC gpio.o stepper.o -o libstepper.so $(LIBS)
 
 stepper.o: stepper.c stepper.h
-	gcc $(CFLAGS) -fPIC -c stepper.c -o stepper.o -lrt
+	gcc $(CFLAGS) -fPIC -c stepper.c -o stepper.o
 
 gpio.o: gpio.c gpio.h
-	gcc $(CFLAGS) -fPIC -c gpio.c -o gpio.o -lrt
+	gcc $(CFLAGS) -fPIC -c gpio.c -o gpio.o
 
 clean:
 	rm -f *.o stepper_control libstepper.so
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 #include "stepper.h"
 
 int main(void) {
@@ -7,9 +8,40 @@ int main(void) {
     stepper_init(&s, 31, 30, 48, 51, 15);
     
     int dir = 0;
-    for(int i = 0; i < 4; i++) {
-        stepper_rot(&s, 180, dir, 10, US_SIXTEENTH);
-        dir = !dir;
+    int rpm = 0;
+    for(int ri = 0; ri < 4; ri++) {
+        rpm += 15;
+        printf("Iteration %d - rpm: %d\n", ri, rpm);
+
+        printf("Full step\n");
+        for(int i = 0; i < 2; i++) {
+            stepper_rot(&s, 180, dir, rpm, US_FULL);
+            dir = !dir;
+        }
+
+        printf("Half step\n");
+        for(int i = 0; i < 2; i++) {
+            stepper_rot(&s, 180, dir, rpm, US_HALF);
+            dir = !dir;
+        }
+
+        printf("Quarter step\n");
+        for(int i = 0; i < 2; i++) {
+            stepper_rot(&s, 180, dir, rpm, US_QUARTER);
+            dir = !dir;
+        }
+
+        printf("Eighth step\n");
+        for(int i = 0; i < 2; i++) {
+            stepper_rot(&s, 180, dir, rpm, US_EIGHTH);
+            dir = !dir;
+        }
+
+        printf("Sixteenth step\n");
+        for(int i = 0; i < 2; i++) {
+            stepper_rot(&s, 180, dir, rpm, US_SIXTEENTH);
+            dir = !dir;
+        }
     }
 
     stepper_destroy(&s);
