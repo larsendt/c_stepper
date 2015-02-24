6ce6757fbf10f6b5b5ccaa799a84c1f0a4f27639
Dane Larsen <dane.t.larsen@gmail.com>
2015-02-24 12:10:37 -0700

    main now checks for init failure

--- main.c
+++ main.c
@@ -5,7 +5,13 @@
 
 int main(void) {
     stepper_control s;
-    stepper_init(&s, 31, 30, 48, 51, 15);
+    int ok = stepper_init(&s, 31, 30, 48, 51, 15);
+
+    if(!ok) {
+        fprintf(stderr, "Failed to initialize stepper\n");
+        return 1;
+    }
+
     stepper_rot(&s, 3600, 0, 60, US_FULL);
     stepper_destroy(&s);
     return 0;
